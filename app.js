45a86c937baaddabd6417b73956efc7c63f0551d
Luigi Henry C. Araujo <87998061+luigihenrydev@users.noreply.github.com>
2025-08-07 20:47:46 -0300

    Adding styling to the menu button

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const toggleBtn = document.getElementById('toggleMenu');
 const sidebar = document.querySelector('.sidebar');
-const mainContent = document.querySelector('.main-content');
 
 toggleBtn.addEventListener('click', () => {
   sidebar.classList.toggle('active');
-  mainContent.classList.toggle('expanded');
 });
